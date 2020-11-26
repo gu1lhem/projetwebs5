82358252d7e4cb22dd81b95fdd38db8d1b3cd25f
gui <gt@anche.no>
2020-11-26 15:13:28 +0100

    Nouveau départ...

--- StudentManager/admin.py
+++ StudentManager/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-from .models import Si_Statut
-from .models import Si_Personnel
-from .models import Si_View_Ls
-
-admin.site.register(Si_Statut)
-admin.site.register(Si_Personnel)
-admin.site.register(Si_View_Ls)
