e4e6987966ac6bf81696c19a4951c1656cf8c6c4
Steven <steven.devek@gmail.com>
2020-11-26 11:55:34 +0100

    model mis a jour

--- StudentManager/models.py
+++ StudentManager/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
+import psycopg2.extension
+
+
 # Create your models here.
+class Individu(models.Model):
+    Prenom = models.CharField(max_length=30)         
+    Nom = models.CharField(max_length=30)  
+    Adressemail = models.CharField(max_length=50)
+    Naiss = models.DateField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
+
+
+class Administratif(Individu):
+    Statut = models.CharField(max_length=30)
+
+
+class Professeur(Individu):
+    Statut = models.CharField(max_length=30)
+    Experience = models.IntegerField()
+
+class Etudiant(Individu):
+    idEtudiant = models.IntegerField(primary_key=True)
+
+class Personnel(Individu):
+    Statut = models.CharField(max_length=30)
+
+
+
+class UniteEnseignement(models.Model):
+    NomMatière = models.CharField(max_length=30)
+    ECTS = models.IntegerField()
+    Type = models.CharField(max_length=15)
+
+class Seance(models.Model):
+    IdSeance = models.IntegerField(primary_key=True)
+    TimecodeDebut = models.DateTimeField(auto_now_add=True)
+    TimecodeFIN = models.DateTimeField(auto_now_add=True)
+    fk_UE = models.ForeignKey(UniteEnseignement, on_delete=models.CASCADE)
+
+class Groupe(models.Model):
+    idgroupe = models.IntegerField(primary_key=True)
+    Libelle = models.CharField(max_length=100)     
+    idniveau = models.CharField(max_length=6)
+    
+class Presence(models.Model):
+    boolPresence = models.BooleanField(default=False)
+    fk_Etudiant = models.ForeignKey(Etudiant, on_delete=models.CASCADE)
+    fk_Seance = models.ForeignKey(Seance, on_delete=models.CASCADE)
+
+
+class Salle(models.Model):
+    Nom = models.IntegerField(primary_key=True)
+    Code = models.CharField(max_length=100)
+    Batiment = models.CharField(max_length=100)
+    Capacite = models.IntegerField()
+    NbPC = models.IntegerField()
+    Projecteur = models.IntegerField()
+    Tableaux = models.IntegerField()
+
+class Semestre(models.Model):
+    DateDebut = models.DateTimeField(auto_now_add=True)
+    NbSemaines = models.IntegerField()
+
+class Formation(models.Model):
+    NomFormation = models.CharField(max_length=100)
+    UFRRattachement = models.CharField(max_length=100,default='Segmi')
--- projectwebs5/settings.py
+++ projectwebs5/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'projectwebs5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projet_jfpp',
+        'USER':'root',
+        'PASSWORD':'',
+        'PORT':'3306'
+    },
+    'sqlite':{
+        'ENGINE':'django.db.backend.sqlite3',
+        'NAME':BASE_DIR/ 'db.sqlite3',
     }
 }
 
