5ecbcc20f0d2db80921f5dc12963839f7e6d6de6
gui <gt@anche.no>
2020-11-27 14:40:51 +0100

    Revert "WIP ajout" erreur de branche
    This reverts commit 9a7b37a5ba978e355c54e06c2bd366f28fba6a3c.

--- StudentManager/forms.py
+++ StudentManager/forms.py
@@ -1,15 +1,1 @@
-from django.forms import Forms, CharField, IntegerField, EmailField, DateTimeField
-
-class ajouterEtudiants(Forms):
-   prenom         = CharField(label="Prénom", required=True, max_length=30)
-   nom            = CharField(label="Nom", required=True, max_length=30)
-   num_etudiant   = IntegerField(label="Numéro étudiant", max_length=8)
-   courriel       = EmailField(label="Adresse de courriel", max_length=60) # EmailField ajoute contrôle de données https://www.geeksforgeeks.org/emailfield-django-forms/
-   date_naissance = DateTimeField()
-
-class ajouterFonctionnaire(Forms):
-   prenom         = CharField(label="Prénom", required=True, max_length=30)
-   nom            = CharField(label="Nom", required=True, max_length=30)
-   status         = CharField(label="Status", required=True, max_length=30)
-   courriel       = EmailField(label="Adresse de courriel", max_length=60)
-   date_naissance = DateTimeField()
+from django import forms
