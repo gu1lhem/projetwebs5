4c9e4fe708f8cd3f8389743dcf20901e99a62541
Malinvaud <Vous@exemple.com>
2021-01-05 11:04:01 +0100

    pré reversible migrate

--- GestionEDT/forms.py
+++ GestionEDT/forms.py
@@ -11,7 +11,7 @@ class ProfesseurModelForm(forms.ModelForm):
 class EtudiantModelForm(forms.ModelForm):
     class Meta:
         model = Etudiant
-        fields = ['NumEtudiant','Prenom','Nom','Adressemail','Naiss']
+        fields = ['NumEtudiant','Prenom','Nom','Adressemail','Naiss','fk_Groupe']
 
 class UEModelForm(forms.ModelForm):
     class Meta:
@@ -26,7 +26,7 @@ class SalleModelForm(forms.ModelForm):
 class SeanceModelForm(forms.ModelForm):
     class Meta:
         model = Seance
-        fields = ['idSeance','TimecodeDebut','TimecodeFIN','fk_UE','fk_Salle']
+        fields = ['idSeance','TimecodeDebut','TimecodeFIN','fk_Professeur','fk_Etudiant','fk_UE','fk_Salle']
 
 class GroupeModelForm(forms.ModelForm):
     class Meta:
@@ -36,12 +36,12 @@ class GroupeModelForm(forms.ModelForm):
 class FormationModelForm(forms.ModelForm):
     class Meta:
         model = Formation
-        fields = ['idFormation','NomFormation','UFRRattachement']
+        fields = ['idFormation','NomFormation','UFRRattachement','fk_Semestre']
 
 class SemestreModelForm(forms.ModelForm):
     class Meta:
         model = Semestre
-        fields = ['NumSemestre','DateDebut','NbSemaines','fk_Formation']
+        fields = ['NumSemestre','DateDebut','NbSemaines']
 
 
 
--- GestionEDT/models.py
+++ GestionEDT/models.py
@@ -7,39 +7,39 @@ from django.db.models import Model, BooleanField, CharField, IntegerField, DateF
 
 # Create your models here.
 class Professeur(models.Model):
-   NumProfesseur = models.IntegerField(primary_key=True)
-   Prenom   = models.CharField(max_length=30)         
-   Nom      = models.CharField(max_length=30)  
-   Adressemail = models.EmailField(max_length=60)
-   Naiss    = models.DateField()
-   Statut = models.CharField(max_length=30)
-   Experience = models.IntegerField()
+   NumProfesseur  = models.IntegerField(primary_key=True)
+   Prenom         = models.CharField(max_length=30)         
+   Nom            = models.CharField(max_length=30)  
+   Adressemail    = models.EmailField(max_length=60)
+   Naiss          = models.DateField()
+   Statut         = models.CharField(max_length=30)
+   Experience     = models.IntegerField()
    def get_absolute_url(self):
-      return reverse("professeur-detail", kwargs={'NumProfesseur': self.NumProfesseur})
+      return reverse("professeur-detail", kwargs={"NumProfesseur": self.NumProfesseur})
 
 class Etudiant(models.Model):
    NumEtudiant = models.IntegerField(primary_key=True)
-   Prenom   = models.CharField(max_length=30)         
-   Nom      = models.CharField(max_length=30)  
+   Prenom      = models.CharField(max_length=30)         
+   Nom         = models.CharField(max_length=30)  
    Adressemail = models.EmailField(max_length=60)
-   Naiss    = models.DateField()
+   Naiss       = models.DateField()
+   fk_Groupe   = models.ForeignKey("Groupe",on_delete=models.CASCADE)
    def get_absolute_url(self):
-      return reverse("etudiant-detail", kwargs={'NumEtudiant': self.NumEtudiant})
+      return reverse("etudiant-detail", kwargs={"NumEtudiant": self.NumEtudiant})
 
 class UniteEnseignement(models.Model):
-   CodeMatiere  = models.CharField(primary_key=True,max_length=30,default=' ')
-   ECTS        = models.IntegerField()
-   Type        = models.CharField(max_length=15)
+   CodeMatiere   = models.CharField(primary_key=True,max_length=30,default=' ')
+   ECTS          = models.IntegerField()
+   Type          = models.CharField(max_length=15)
    def get_absolute_url(self):
-      return reverse("ue-detail", kwargs={'CodeMatiere': self.CodeMatiere})
-
+      return reverse("ue-detail", kwargs={"CodeMatiere": self.CodeMatiere})
 
 class Salle(models.Model):
-   Nom      = models.IntegerField(primary_key=True)
-   Code     = models.CharField(max_length=100)
-   Batiment = models.CharField(max_length=100)
-   Capacite = models.IntegerField()
-   NbPC     = models.IntegerField()
+   Nom         = models.IntegerField(primary_key=True)
+   Code        = models.CharField(max_length=100)
+   Batiment    = models.CharField(max_length=100)
+   Capacite    = models.IntegerField()
+   NbPC        = models.IntegerField()
    Projecteur  = models.IntegerField()
    Tableaux    = models.IntegerField()
    def get_absolute_url(self):
@@ -49,6 +49,8 @@ class Seance(models.Model):
    idSeance       = models.IntegerField(primary_key=True)
    TimecodeDebut  = models.DateTimeField()
    TimecodeFIN    = models.DateTimeField()
+   fk_Professeur  = models.ForeignKey(Professeur,on_delete=models.CASCADE)
+   fk_Etudiant    = models.ForeignKey(Etudiant,on_delete=models.CASCADE)
    fk_UE          = models.ForeignKey(UniteEnseignement, on_delete=models.CASCADE)
    fk_Salle       = models.ForeignKey(Salle,on_delete=models.CASCADE)
    def get_absolute_url(self):
@@ -65,6 +67,7 @@ class Formation(models.Model):
    idFormation = models.IntegerField(primary_key=True)
    NomFormation = models.CharField(max_length=100)
    UFRRattachement = models.CharField(max_length=100,default='SEGMI')
+   fk_Semestre = models.ForeignKey("Semestre",on_delete=models.CASCADE) 
    def get_absolute_url(self):
       return reverse("formation-detail", kwargs={"idFormation": self.idFormation})
 
@@ -72,7 +75,6 @@ class Semestre(models.Model):
    NumSemestre = models.IntegerField(primary_key=True)
    DateDebut   = models.DateTimeField()
    NbSemaines  = models.IntegerField()
-   fk_Formation = models.ForeignKey(Formation,on_delete=models.CASCADE) 
    def get_absolute_url(self):
       return reverse("semestre-detail", kwargs={"NumSemestre": self.NumSemestre})
 
