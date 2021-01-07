2d1834cdd83875ffa05084f0406973ea35234333
gui <gt@anche.no>
2021-01-07 23:08:17 +0100

    static scripts

--- Projetweb/settings.py
+++ Projetweb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#--------------------------------------------------
+#STATIC_ROOT = os.path.join(BASE_DIR, 'root') #! probablement inutile
+#-----------------------------------------------------
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   #os.path.join(BASE_DIR, 'boot'),  #! probablement inutile
+]
