11ccc51f1b4f191d85d4045f0d4a18e1d303cdb5
Malinvaud <Vous@exemple.com>
2021-01-19 00:03:26 +0100

    déploiement et ménage

--- Projetweb/urls.py
+++ Projetweb/urls.py
@@ -25,7 +25,7 @@ from GestionEDT.views import *
 # Bootstrap Crud Template
 from bsct.urls import URLGenerator
 
-bsct_patterns = URLGenerator( Professeur ).get_urlpatterns(crud_types = 'crudl')
+bsct_patterns_p = URLGenerator( Professeur ).get_urlpatterns(crud_types = 'crudl')
 """ Génération automatique de toutes les URLs de création, détails, etc.
    #'c' - Refers to the Create CRUD type
    #'r' - Refers to the Read/Detail CRUD type
@@ -34,57 +34,19 @@ bsct_patterns = URLGenerator( Professeur ).get_urlpatterns(crud_types = 'crudl')
    #'l' - Refers to the List CRUD type
 """
 
+bsct_patterns_e = URLGenerator( Etudiant ).get_urlpatterns(crud_types = 'crudl')
+bsct_patterns_u = URLGenerator( UC ).get_urlpatterns(crud_types = 'crudl')
+bsct_patterns_sa = URLGenerator( Salle ).get_urlpatterns(crud_types = 'crudl')
+bsct_patterns_se = URLGenerator( Seance ).get_urlpatterns(crud_types = 'crudl')
+bsct_patterns_g = URLGenerator( Groupe ).get_urlpatterns(crud_types = 'crudl')
+bsct_patterns_f = URLGenerator( Formation ).get_urlpatterns(crud_types = 'crudl')
+
 urlpatterns = [
-   url( '', include( bsct_patterns ) )
+   url( '', include( bsct_patterns_p ) ) ,
+   url( '', include( bsct_patterns_e ) ) , 
+   url( '', include( bsct_patterns_u ) ) , 
+   url( '', include( bsct_patterns_sa ) ) , 
+   url( '', include( bsct_patterns_se ) ) , 
+   url( '', include( bsct_patterns_g ) ) , 
+   url( '', include( bsct_patterns_f ) )
 ]
-
-"""
-urlpatterns = [
-   # Index
-   path('', home, name = 'homepage'),
-   path('index/', home, name = 'homepage'),
-   path('home/', home, name = 'homepage'),
-
-   path('admin/', admin.site.urls),
-
-   path('etudiant/', EtudiantList.as_view(), name='etudiant-list'),
-   path('etudiant/add/',EtudiantCreate.as_view(),name='etudiant-add'),
-   path('etudiant/<int:NumEtudiant>/', EtudiantDetail.as_view(), name='etudiant-detail'),
-   path('etudiant/<int:NumEtudiant>/delete/',EtudiantDelete.as_view(),name='etudiant-delete'),
-   path('etudiant/<int:NumEtudiant>/update/',EtudiantUpdate.as_view(),name='etudiant-update'),
-
-   path('UC/', UCList.as_view(), name='uc-list'),
-   path('UC/add/', UCCreate.as_view(), name='uc-add'),
-   path('UC/<int:idUC>/', UCDetail.as_view(), name='uc-detail'),
-   path('UC/<int:idUC>/update/', UCUpdate.as_view(), name='uc-update'),
-   path('UC/<int:idUC>/delete/', UCDelete.as_view(), name='uc-delete'),
-
-   path('salle/', SalleList.as_view(), name='salle-list'),
-   path('salle/add/', SalleCreate.as_view(), name='salle-add'),
-   path('salle/<int:idSalle>/', SalleDetail.as_view(), name='salle-detail'), # clés primaires de 
-   path('salle/<int:idSalle>/update/', SalleUpdate.as_view(), name='salle-update'), # salle ?
-   path('salle/<int:idSalle>/delete/', SalleDelete.as_view(), name='salle-delete'),
-
-   path('seance/', SeanceList.as_view(), name='seance-list'),
-   path('seance/add/', SeanceCreate.as_view(), name='seance-add'),
-   path('seance/<int:idSeance>/', SeanceDetail.as_view(), name='seance-detail'),
-   path('seance/<int:idSeance>/update/', SeanceUpdate.as_view(), name='seance-update'),
-   path('seance/<int:idSeance>/delete/', SeanceDelete.as_view(), name='seance-delete'),
-
-   path('groupe/', GroupeList.as_view(), name='groupe-list'),
-   path('groupe/add/', GroupeCreate.as_view(), name='groupe-add'),
-   path('groupe/<int:idGroupe>/', GroupeDetail.as_view(), name='groupe-detail'),
-   path('groupe/<int:idGroupe>/update/', GroupeUpdate.as_view(), name='groupe-update'),
-   path('groupe/<int:idGroupe>/delete/', GroupeDelete.as_view(), name='groupe-delete'),
-
-   path('formation/', FormationList.as_view(), name='formation-list'),
-   path('formation/add/', FormationCreate.as_view(), name='formation-add'),
-   path('formation/<int:idFormation>/', FormationDetail.as_view(), name='formation-detail'),
-   path('formation/<int:idFormation>/update/', FormationUpdate.as_view(), name='formation-update'),
-   path('formation/<int:idFormation>/delete/', FormationDelete.as_view(), name='formation-delete'),
-]
-
-urlpatterns += [
-   path('accounts/', include('django.contrib.auth.urls')),
-]
-"""
