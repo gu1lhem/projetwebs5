74935f3c4a1a23df149fbdb728f3fe2274002d59
Malinvaud <paul.3rst@gmail.com>
2021-01-26 10:41:04 +0100

    WIP: intégration models of GestionEDT in wagtail with ParentalKey

--- EditEDT/models.py
+++ EditEDT/models.py
@@ -1,12 +1,11 @@
-from operator import add
 from django.db import models
-from django.db.models import Model, BooleanField, CharField, IntegerField, DateField, DateTimeField, DecimalField, ForeignKey, EmailField
-
+from modelcluster.fields import ParentalKey
 from ls.joyous.models import (MultidayEventPage, RecurringEventPage,
                               MultidayRecurringEventPage, removeContentPanels,
                               )
 from ls.joyous.models.one_off_events import SimpleEventPage
-from wagtail.admin.edit_handlers import MultiFieldPanel
+from wagtail.admin.edit_handlers import MultiFieldPanel, FieldPanel, InlinePanel
+from wagtail.core.models import Orderable
 from GestionEDT.models import *
 
 # Hide unwanted event types
@@ -16,7 +15,18 @@ MultidayRecurringEventPage.is_creatable = False
 
 #Intégration de champ relatif à nos models Professeur, Groupe d'étudiants, etc..
 #SimpleEventPage.content_panels += [MultiFieldPanel('Professeur')]
+class SimpleRelatedEventPage(Orderable,SimpleEventPage):
+    id_seance = models.AutoField("Identifiant de la séance",primary_key=True)   
+    professeur = ParentalKey(Professeur, on_delete=models.CASCADE, related_name='professeur')
+    groupe = ParentalKey(Groupe,on_delete=models.CASCADE, related_name='groupe')
+    salle = ParentalKey(Salle, on_delete=models.CASCADE, related_name='salle')
+    uc = ParentalKey(UC,on_delete=models.CASCADE, related_name='matière')
 
-# Hide unwanted content
-removeContentPanels(["image","category", "tz", "group_page", "website"])
-# Create your models here.
+    content_panels = [
+    InlinePanel('professeur', label="Professeur animant le cours"),
+    InlinePanel('groupe', label="Groupe assistant au cours"),
+    InlinePanel('salle', label="Salle dans laquelle se déroule le cours"),
+    InlinePanel('matière', label="Matière du cours"),
+]
+
+
